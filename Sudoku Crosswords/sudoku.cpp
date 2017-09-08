ea1158d25ba6bc9581c7ec9aa9717d36b72a2507
kzy-pan <panyoulin@163.com>
2017-09-08 16:47:59 +0800

    feat:add check the repeat
    Through using a tree,check whether there is a repeat of the disk.

--- Sudoku Crosswords/sudoku.cpp
+++ Sudoku Crosswords/sudoku.cpp
@@ -5,22 +5,30 @@
 #include<ctime>
 #include<cstring>
 using namespace std;
-ofstream outfile("shudu.txt",ios::out);//用于输出的文件流 
+struct Nod
+{
+	struct Nod* next[9];
+};
+ofstream outfile("sudoku.txt",ios::out);//用于输出的文件流 
 int main(int argc, char** argv)
 {	
 	void ShuffleArray_Fisher_Yates(char* arr, int len);//用于生产字符数组的乱序 
 	void print_shudo(char arr[][9]);//将生成的数独终盘传入文件中 
-	void generator_shudo();//用于生成数独终盘 
+	void generator_shudo(struct Nod* r);//用于生成数独终盘 
 	bool isNum(string str);//判断是否为纯数字，用于参数传入
+	bool check(char arr[][3],struct Nod* r,char ch[4]);
 	int tmp;
-	int j=0;	
+	int j=0;
+	struct Nod* root =new struct Nod;
+	for(int i =0;i<9;i++)
+		root->next[i]=NULL;
 	if(!outfile)
 	{//文件打开失败以及提示 
 		cerr<<"open error!"<<endl;
 		exit(1);
 	}
 	srand(time(0));//设置随机种子 
-	if(argv[1]) 
+	if(argv[1]&&argc<=3) 
 	{
 		if(!strcmp(argv[1],"-c"))//对于参数格式的判断 
 		{
@@ -30,7 +38,7 @@ int main(int argc, char** argv)
 				j=atoi(str.c_str());	
 				for(int i=0;i<j;i++)
 				{
-					generator_shudo();
+					generator_shudo(root);
 				}
 			}
 			else cerr<<"bad input"<<endl;
@@ -39,7 +47,7 @@ int main(int argc, char** argv)
 			cerr<<"need '-c'"<<endl;
 	}
 	else
-		cerr<<""<<endl;
+		cerr<<"The wrong format"<<endl;
 	outfile.close();
 	cout<<"finish"<<endl;
 	return 0; 
@@ -72,8 +80,45 @@ void print_shudo(char arr[][9])
 	}
 	outfile<<endl;
 }
-void generator_shudo()
+bool check(char arr[][3],struct Nod* r,char ch[4])
 {
+	struct Nod* p=r;
+	bool jd=0;
+	for(int i=1;i<9;i++)
+	{
+		int j=arr[i/3][i%3]-'0'; 
+		if(p->next[j-1])
+			p=p->next[j-1];
+		else
+		{
+			jd=1;
+			struct Nod* q=new struct Nod;
+			for(int i =0;i<9;i++)
+				q->next[i]=NULL;
+			p->next[j-1]=q;
+			p=q;
+		}
+	}
+	for(int i=0;i<4;i++)
+	{
+		int j=ch[i];
+		if(p->next[j])
+			p=p->next[j];
+		else
+		{
+			jd=1;
+			struct Nod* q=new struct Nod;
+			for(int i =0;i<9;i++)
+				q->next[i]=NULL;
+			p->next[j]=q;
+			p=q;
+		}
+	}
+	return jd;
+}
+void generator_shudo(struct Nod* r)
+{
+	char ch[4];
 	char shudo[9][9];
 	char mini[3][3];
 	for(int i=0;i<9;i++)	
@@ -81,7 +126,10 @@ void generator_shudo()
 			shudo[i][j]='0';
 	shudo[0][0]='2';
 	mini[0][0]='2';//初始化 
-	char a[8]={'1','3','4','5','6','7','8','9'};	
+	char a[8]={'1','3','4','5','6','7','8','9'};
+	while(1)
+	{
+	a[0]='1';a[1]='3';a[2]='4';a[3]='5';a[4]='6';a[5]='7';a[6]='8';a[7]='9';		
 	ShuffleArray_Fisher_Yates(a,8);//得到乱序用于填充第一宫 
 	for(int i=1;i<9;i++)
 	{
@@ -121,6 +169,7 @@ void generator_shudo()
 	for(int j=1;j<3;j++)
 	{
 		int jd=rand()%4;
+		ch[j-1]=jd;
 		if(jd)
 			for(int i=0;i<9;i++)
 			{
@@ -132,6 +181,7 @@ void generator_shudo()
 	for(int j=1;j<3;j++)
 	{
 		int jd=rand()%4;
+		ch[j+1]=jd;
 		if(jd)
 			for(int i=0;i<9;i++)
 			{
@@ -139,7 +189,9 @@ void generator_shudo()
 				shudo[i][jd-1+j*3]=shudo[i][(jd>2?0:jd)+j*3];
 				shudo[i][(jd>2?0:jd)+j*3]=tmp;
 			}
-	}//对于第4-6列，第7-9列进行对调 
+	}//对于第4-6列，第7-9列进行对调
+	if(check(mini,r,ch)) break;
+	}
 	print_shudo(shudo);
 }
 bool isNum(string str)  
